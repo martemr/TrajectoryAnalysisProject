9e26069e25f73c32d7784d41b6dda4c8b211e550
M. Martin EMERY <martin.emery@cerema.fr>
2023-03-15 13:25:30 +0100

    Suppressions code inutile

--- code/stopTime.R
+++
@@ -1,8 +0,0 @@
-plot(tracks$xCenter,tracks$yCenter)
-df <- data.frame('x'=round(tracks$xCenter),'y'=round(tracks$yCenter))
-
-df_counts <- table(df$x, df$y)
-
-# create heatmap
-heatmap(df_counts, Rowv = NA, Colv = NA, col = heat.colors(256), 
-        margins = c(5, 5), main = "Heatmap of (x, y) occurrences")
