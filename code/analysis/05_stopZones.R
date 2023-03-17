3657f5750a2715b6668be529ea93e07bb9897341
M. Martin EMERY <martin.emery@cerema.fr>
2023-03-17 16:07:07 +0100

    Heatmap des zones de stop

--- code/analysis/05_stopZones.R
+++ code/analysis/05_stopZones.R
@@ -33,6 +33,24 @@ speedArray <- data.frame('trackId'= speedArrayDataset$trackId,
                          'Speed'  = as.integer(getSpeed(speedArrayDataset$xVelocity,
                                                         speedArrayDataset$yVelocity)))
 
+# for (cId in unique(clusters$clusterId)){
+#   clusterSpeedArray <- speedArray[trackId %in% unlist(clusters[clusterId==cId, 'trackId']),]
+  
+  myarray <- aggregate(Speed ~ xCenter + yCenter, data=speedArray, FUN=mean)
+  matToPlot <-  matrix(0, 
+                       nrow=abs(xlim[2]+1-xlim[1]), 
+                       ncol=abs(ylim[2]+1-ylim[1]), 
+                       dimnames=list(seq(xlim[1], xlim[2]), seq(ylim[1], ylim[2])))
+  
+  for (row in seq(1,nrow(myarray))){
+    matToPlot[rownames(matToPlot)==myarray[row,'xCenter'],colnames(matToPlot)==myarray[row,'yCenter']] <- myarray[row,'Speed']
+  }
+  
+  # N'affiche pas les valeurs nulles
+  matToPlot[matToPlot==0]<- NA
+  printHeatMapMatrix(mat = matToPlot, name =paste("Vitesses pratiquées cluster",cId ), subTitle = paste(0, "%des véhicules commettent un excès de vitesse"))
+#}
+
 #==========================================
 # Draw stop zones
 #==========================================
