222c08237f7cd3145851f51767393cdb9407d6b3
M. Martin EMERY <martin.emery@cerema.fr>
2023-04-25 14:36:00 +0200

    Speed study

--- code/analysis/01_speedStudy.R
+++ code/analysis/01_speedStudy.R
@@ -41,29 +41,90 @@ getInfractionRate <- function(cId){
 #==========================================
 # Analysis of the speed
 #==========================================
-speedArrayDataset <- trajectoriesDataset[trajectoriesDataset$class==StudiedClass,]
-speedArray <- data.table('trackId'= speedArrayDataset$trackId,
-                         'xCenter'= round(speedArrayDataset$xCenter,0), 
-                         'yCenter'= round(speedArrayDataset$yCenter,0), 
-                         'Speed'  = as.integer(getSpeed(speedArrayDataset$xVelocity,
-                                                        speedArrayDataset$yVelocity)))
+library(plotly)
+s <- matrix(c(1, -.75, -.75, 1), ncol = 2)
 
-for (cId in unique(clusters$clusterId)){
-  clusterSpeedArray <- speedArray[trackId %in% unlist(clusters[clusterId==cId, 'trackId']),]
+obs <- mvtnorm::rmvnorm(500, sigma = s)
+
+m <- ggplot(trajectoriesDataset[,.(xCenter,yCenter)]) +
+  geom_point() +
+  xlim(0.5, 6) +
+  ylim(40, 110)
+m + geom_density_2d()
+
+# library
+library(latticeExtra) 
+
+# create data
+set.seed(1) 
+data.frame(x = rnorm(100), y = rnorm(100)) 
+data <- trajectoriesDataset[1:100, .(xCenter, yCenter, xVelocity)]
+
+# showing data points on the same color scale 
+levelplot(xVelocity ~ xCenter * yCenter, data, 
+          panel = panel.levelplot.points, cex = 1.2
+) + 
+  layer_(panel.2dsmoother(..., n = 200))
+
+
+geom_density_2d(data = trajectoriesDataset[,.(xCenter,yCenter)])
+fig <- plot_ly(trajectoriesDataset[,.(xCenter,yCenter)], xlim=c(0,100)) #x = obs[,1], y = obs[,2]) 
+fig <- fig %>% add_trace(type='histogram2dcontour')
+
+
+fig
+
+
+createSpeedHeatMap <- function(input,output){
+  myarray <- aggregate(Speed ~ xCenter + yCenter, data=trajectoriesDataset[, Speed:=getSpeed(xCenter,yCenter)], FUN=mean)
   
-  myarray <- aggregate(Speed ~ xCenter + yCenter, data=clusterSpeedArray, FUN=mean)
   matToPlot <-  matrix(0, 
                        nrow=abs(xlim[2]+1-xlim[1]), 
                        ncol=abs(ylim[2]+1-ylim[1]), 
                        dimnames=list(seq(xlim[1], xlim[2]), seq(ylim[1], ylim[2])))
   
+  matToPlot <-  matrix()
   for (row in seq(1,nrow(myarray))){
     matToPlot[rownames(matToPlot)==myarray[row,'xCenter'],colnames(matToPlot)==myarray[row,'yCenter']] <- myarray[row,'Speed']
   }
   
   # N'affiche pas les valeurs nulles
   matToPlot[matToPlot==0]<- NA
-  printHeatMapMatrix(mat = matToPlot, name =paste("Vitesses pratiquées cluster",cId ), subTitle = paste(getInfractionRate(cId), "%des véhicules commettent un excès de vitesse"))
+  printHeatMapMatrix(mat = matToPlot, name =paste("Vitesses pratiquées cluster"))
+  
+  library(latticeExtra)
+  
+  
+  
+  speedArray <- trajectoriesDataset[]
+  
+  
+  
+  speedArrayDataset <- trajectoriesDataset[trajectoriesDataset$class==StudiedClass,]
+  speedArray <- data.table('trackId'= speedArrayDataset$trackId,
+                           'xCenter'= round(speedArrayDataset$xCenter,0), 
+                           'yCenter'= round(speedArrayDataset$yCenter,0), 
+                           'Speed'  = as.integer(getSpeed(speedArrayDataset$xVelocity,
+                                                          speedArrayDataset$yVelocity)))
+  
+  for (cId in unique(clusters$clusterId)){
+    clusterSpeedArray <- speedArray[trackId %in% unlist(clusters[clusterId==cId, 'trackId']),]
+    
+    myarray <- aggregate(Speed ~ xCenter + yCenter, data=clusterSpeedArray, FUN=mean)
+    matToPlot <-  matrix(0, 
+                         nrow=abs(xlim[2]+1-xlim[1]), 
+                         ncol=abs(ylim[2]+1-ylim[1]), 
+                         dimnames=list(seq(xlim[1], xlim[2]), seq(ylim[1], ylim[2])))
+    
+    for (row in seq(1,nrow(myarray))){
+      matToPlot[rownames(matToPlot)==myarray[row,'xCenter'],colnames(matToPlot)==myarray[row,'yCenter']] <- myarray[row,'Speed']
+    }
+    
+    # N'affiche pas les valeurs nulles
+    matToPlot[matToPlot==0]<- NA
+    printHeatMapMatrix(mat = matToPlot, name =paste("Vitesses pratiquées cluster",cId ), subTitle = paste(getInfractionRate(cId), "%des véhicules commettent un excès de vitesse"))
+  }
+  
 }
 
 
