780e035320366934d5a1f60a63abc9bd6fef98a1
M. Martin EMERY <martin.emery@cerema.fr>
2023-05-17 16:09:44 +0200

    corrections erreurs matrice P

--- code/analysis/conflictAnalyse/02_trajectoryPrediction.R
+++ code/analysis/conflictAnalyse/02_trajectoryPrediction.R
@@ -15,18 +15,22 @@ predictKalmanPosition <- function(studiedTrackId, startFrame,y){
   dt=5/recordingMeta[recordingId==unique(trajectoriesDataset[trackId==studiedTrackId, recordingId]), frameRate] # Intervalle de temps
   
   # x : Vecteur d'état
-  data <- trajectoriesDataset[trackId==studiedTrackId & frame==startFrame, 
+  data <- trajectoriesDataset[trackId==studiedTrackId & frame>startFrame, 
                               .(xCenter,yCenter,xVelocity,yVelocity,xAcceleration,yAcceleration)]
-  x0 = matrix(unlist(data), nrow=1)
+  x0 = matrix(unlist(data[1]), nrow=1)
   
   # P : Matrice de covariance
-  P0matrix=as.matrix(rbind(c(getVariancePercentile(trackClass,'xPosition',0.75,1),   0,0,    0,  0,   0),
-                        c(   0,getVariancePercentile(trackClass,'yPosition',0.75,1),0,    0,  0,   0),
-                        c(   0,   0,getVariancePercentile(trackClass,'xSpeed',0.75,2),    0,  0,   0),
-                        c(   0,   0,    0,getVariancePercentile(trackClass,'ySpeed',0.75,2),  0,   0), 
-                        c(   0,   0,    0,    0,getVariancePercentile(trackClass,'xAcc',0.75,3),   0),
-                        c(   0,   0,    0,    0,    0,getVariancePercentile(trackClass,'yAcc',0.75,3)))
-  )
+  # P est l'estimation de l'erreur de prédiction du modèle. On s'inspire de l'erreur réelle dans nos mesures pour le calculer.
+  errors <- fread(sprintf("%serrorsPositionKalman.csv",dosinit), header = TRUE, sep = ',')
+  err=unlist(errors[unique(trajectoriesDataset[trackId==studiedTrackId,locationId]),])[switch(unique(trajectoriesDataset[trackId==studiedTrackId,class]),                                                                                             'car'=1,'truck_bus'=2,'pedestrian'=3,'bicycle'=4)]
+  err=1
+  P0matrix = as.matrix(rbind(c(err,  0,      0,      0,             0,              0),
+                             c(0,  err,      0,      0,             0,              0), 
+                             c(0,    0, err*dt,      0,             0,              0),
+                             c(0,    0,      0, err*dt,             0,              0),
+                             c(0,    0,      0,      0,((err*dt)^2)/2,              0),
+                             c(0,    0,      0,      0,             0,((err*dt)^2)/2)))
+  
   
   # F : Matrice de transition
   Fmatrix= as.matrix(rbind(c(1, 0,dt, 0,1/2*dt^2,       0),
@@ -37,23 +41,25 @@ predictKalmanPosition <- function(studiedTrackId, startFrame,y){
                            c(0, 0, 0, 0,       0,       1)))
   
   # Q : Bruit de modèle
-  # On s'inspire du vrai bruit : Calculé précédemment
-  errors <- fread(sprintf("%serrorsPositionKalman.csv",dosinit), header = TRUE, sep = ',')
-  err=unlist(errors[unique(trajectoriesDataset[trackId==studiedTrackId,locationId]),])[switch(unique(trajectoriesDataset[trackId==studiedTrackId,class]), 
-                            'car'=1,'truck_bus'=2,'pedestrian'=3,'bicycle'=4)]
-  Qmatrix= as.matrix(rbind(c(err,  0,      0,      0,             0,              0),
-                           c(0,  err,      0,      0,             0,              0), 
-                           c(0,    0, err*dt,      0,             0,              0),
-                           c(0,    0,      0, err*dt,             0,              0),
-                           c(0,    0,      0,      0,((err*dt)^2)/2,              0),
-                           c(0,    0,      0,      0,             0,((err*dt)^2)/2)))
+  # On considère que notre modèle est fiable donc faible bruit 
+  Qmatrix= as.matrix(rbind(c(10^(-3),  0,          0,          0,                 0,                 0),
+                           c(0,  10^(-3),          0,          0,                 0,                 0), 
+                           c(0,        0, 10^(-3)*dt,          0,                 0,                 0),
+                           c(0,        0,          0, 10^(-3)*dt,                 0,                 0),
+                           c(0,        0,          0,          0,((10^(-3)*dt)^2)/2,                 0),
+                           c(0,        0,          0,          0,                 0,((10^(-3)*dt)^2)/2)))
   
   # R : Bruit de mesure
   # On le considère égal à Q
   Rmatrix = Qmatrix
   
   # H : Processus de mesure
-  Hmatrix = matrix(c(1,1,0,0,0,0), nrow=1)
+  Hmatrix = as.matrix(rbind(c(1, 0, 0, 0, 0, 0),
+                            c(0, 1, 0, 0, 0, 0), 
+                            c(0, 0, 0, 0, 0, 0),
+                            c(0, 0, 0, 0, 0, 0),
+                            c(0, 0, 0, 0, 0, 0),
+                            c(0, 0, 0, 0, 0, 0)))
 
   
   # Appel au filtre de kalman  
