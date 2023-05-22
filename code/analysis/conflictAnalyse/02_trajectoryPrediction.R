c56b77ac8235a6fb2befb2813a965a8ad364ccff
M. Martin EMERY <martin.emery@cerema.fr>
2023-05-22 12:41:55 +0200

    Corrections toutes erreurs Kalman !

--- code/analysis/conflictAnalyse/02_trajectoryPrediction.R
+++ code/analysis/conflictAnalyse/02_trajectoryPrediction.R
@@ -10,16 +10,16 @@
 #==========================================
 # Filtre de kalman
 #==========================================
-predictKalmanPosition <- function(studiedTrackId, startFrame,y){
+predictKalmanPosition <- function(studiedTrackId, startFrame,yt){
   # On définit les variables du filtre de kalamn
   dt=5/recordingMeta[recordingId==unique(trajectoriesDataset[trackId==studiedTrackId, recordingId]), frameRate] # Intervalle de temps
   
-  # x : Vecteur d'état
+  # x [6*1] : Vecteur d'état
   data <- trajectoriesDataset[trackId==studiedTrackId & frame>startFrame, 
                               .(xCenter,yCenter,xVelocity,yVelocity,xAcceleration,yAcceleration)]
-  x0 = matrix(unlist(data[1]), nrow=1)
+  x0 = matrix(unlist(data[1]), ncol=1)
   
-  # P : Matrice de covariance
+  # P [6*6] : Matrice de covariance
   # P est l'estimation de l'erreur de prédiction du modèle. On s'inspire de l'erreur réelle dans nos mesures pour le calculer.
   errors <- fread(sprintf("%serrorsPositionKalman.csv",dosinit), header = TRUE, sep = ',')
   err=unlist(errors[unique(trajectoriesDataset[trackId==studiedTrackId,locationId]),])[switch(unique(trajectoriesDataset[trackId==studiedTrackId,class]),                                                                                             'car'=1,'truck_bus'=2,'pedestrian'=3,'bicycle'=4)]
@@ -32,7 +32,7 @@ predictKalmanPosition <- function(studiedTrackId, startFrame,y){
                              c(0,    0,      0,      0,             0,((err*dt)^2)/2)))
   
   
-  # F : Matrice de transition
+  # F [6*6] : Matrice de transition
   Fmatrix= as.matrix(rbind(c(1, 0,dt, 0,1/2*dt^2,       0),
                            c(0, 1, 0,dt,       0,1/2*dt^2), 
                            c(0, 0, 1, 0,      dt,       0),
@@ -40,7 +40,7 @@ predictKalmanPosition <- function(studiedTrackId, startFrame,y){
                            c(0, 0, 0, 0,       1,       0),
                            c(0, 0, 0, 0,       0,       1)))
   
-  # Q : Bruit de modèle
+  # Q [6*6] : Bruit de modèle
   # On considère que notre modèle est fiable donc faible bruit 
   Qmatrix= as.matrix(rbind(c(10^(-3),  0,          0,          0,                 0,                 0),
                            c(0,  10^(-3),          0,          0,                 0,                 0), 
@@ -49,19 +49,24 @@ predictKalmanPosition <- function(studiedTrackId, startFrame,y){
                            c(0,        0,          0,          0,((10^(-3)*dt)^2)/2,                 0),
                            c(0,        0,          0,          0,                 0,((10^(-3)*dt)^2)/2)))
   
-  # R : Bruit de mesure
-  # On le considère égal à Q
-  Rmatrix = Qmatrix
+  # R [2*2] : Bruit de mesure
+  Rmatrix = as.matrix(rbind(c(10^(-3),       0),
+                            c(0      , 10^(-3))))
   
-  # H : Processus de mesure
-  Hmatrix = as.matrix(rbind(c(1, 0, 0, 0, 0, 0),
-                            c(0, 1, 0, 0, 0, 0), 
-                            c(0, 0, 0, 0, 0, 0),
-                            c(0, 0, 0, 0, 0, 0),
-                            c(0, 0, 0, 0, 0, 0),
-                            c(0, 0, 0, 0, 0, 0)))
+  # H [2*6] : Processus de mesure
+  Hmatrix = as.matrix(rbind(c(1, 0, dt,  0, 0.5*(dt)^2,          0),
+                            c(0, 1,  0, dt,          0, 0.5*(dt)^2)))
 
   
+  # yt [2*T] : Observations 
+  yt = yt # t(trajectoriesDataset[trackId==7,.(xCenter,yCenter)][1:10,])
+  
   # Appel au filtre de kalman  
-  kalmanFilter(x0,P0matrix,y,Fmatrix,Qmatrix, Rmatrix, Hmatrix)
+  kalmanFilter(x0=x0,
+               P0 = P0matrix,
+               yt = yt,
+               Fm = Fmatrix,
+               Qm = Qmatrix, 
+               Rm = Rmatrix, 
+               Hm = Hmatrix)
 }
--- code/prediction/01_kalmanFilter.R
+++ code/prediction/01_kalmanFilter.R
@@ -10,68 +10,79 @@ library(matlib)
 
 #==========================================
 # Filtre de kalman
-# y est une liste de vecteurs
+# Avec : 
+#  - Ny the number of variables 
+#  - Nb the number of unobserved variables in the state equation
+#  - T  the number of time periods
+# x0 TAILLE:[Nb*1] for the initial guess of the unobserved components 
+# P0 TAILLE:[Nb*Nb] for the initial guess of the unobserved components covariance matrix, 
+# yt TAILLE:[Ny*T]  est une liste de vecteurs 
+# Dm TAILLE:[Nb*1]  for the constant in the state equation, 
+# Am TAILLE:[Ny*1]  for the constant in the observation equation, 
+# Fm TAILLE:[Nb*Nb] for the state equation transition matrix, 
+# Hm TAILLE:[Ny*Nb] for the observation matrix in the observation equation, 
+# Qm TAILLE:[Nb*Nb] for the covariance matrix of the errors in the state equation, 
+# Rm TAILLE:[Ny*Ny]  for the covariance matrix of the errors in the observation equation,
 #==========================================
-kalmanFilter <- function(x0,P0matrix,y,Fmatrix,Qmatrix, Rmatrix, Hmatrix){
 
-  # nombre de mesures
-  n = length(y)
-  m=nrow(y)
+x0 = x0
+P0 = P0
+yt = yt
+Fm = Fmatrix
+Hm = Hmatrix
+Qm = Qmatrix
+Rm = Rmatrix
 
-  # variables initiales
-  x_k_k <- as.vector(x0)
-  P_k_k <- P0matrix 
-  I <- diag(n)
+kalmanFilter <- function(x0,P0,yt,Fm,Qm,Rm,Hm){
+
+  # Nombre de mesures
+  Tmps = ncol(yt)
+  Ny = nrow(yt)
+  Nb = nrow(x0)
   
-  P_values <- list(P0matrix)
-  #x_values <- list(x0)
-  x_values   <- data.table(x=x0[1], 
-                           y=x0[2],
-                           xVelocity=0,
-                           yVelocity=0,
-                           xAcceleration=0,
-                           yAcceleration=0)
-  #xkk_values   <- data.table(x=x0[1], y=x0[2])
-  #xk1k_values  <- data.table(x=x0[1], y=x0[2])
-  #xk1k1_values <- data.table(x=x0[1], y=x0[2])
+  # Verification des dimensions
+  stopifnot('Dimensions de P0 incorrectes' = dim(P0)==c(Nb,Nb))
+  stopifnot('Dimensions de Fm incorrectes' = dim(Fm)==c(Nb,Nb))
+  stopifnot('Dimensions de Qm incorrectes' = dim(Qm)==c(Nb,Nb))
+  stopifnot('Dimensions de Rm incorrectes' = dim(Rm)==c(Ny,Ny))
+  stopifnot('Dimensions de Hm incorrectes' = dim(Hm)==c(Ny,Nb))
+  
+  # variables initiales
+  x_k_k <- x0
+  P_k_k <- P0
+  I <- diag(Nb) 
+  
+  # Préparation des variables de memoire
+  P_t1 <- list(P0)
+  x_t1 <- list(x0)
+  P_tt <- list()
+  x_tt <- list()
   
   # CACUL DU FILTRE
-  for (i in seq(1,m)){
+  for (i in seq(1,Tmps)){
     # Prediction
-    x_k1_k <- as.vector(Fmatrix %*% x_k_k)
-    P_k1_k <- Fmatrix %*% P_k_k %*% t(Fmatrix) + Qmatrix 
-
-    # Correction
-    y_k1 <- unlist(y[i])
-    K_k1 <- (P_k1_k %*% Hmatrix) %*% inv(Hmatrix %*% P_k1_k %*% Hmatrix + Rmatrix) # Ici H n'est pas transposé car R comprend seul dans quel sens le lire (vecteur colonne ou ligne)
-    print(K_k1)
-    x_k1_k1 <- as.vector(x_k1_k + K_k1 %*% (y_k1 - as.vector(Hmatrix %*% x_k1_k)))
-    P_k1_k1 <- (I - as.numeric(K_k1 %*% Hmatrix)) %*% P_k1_k
-
-    # Stockage des résultats
-    P_values <- append(P_values, list(P_k1_k1))
-    # x_values <- append(x_values, list(x_k1_k1))
-    x_values <- rbind(x_values, list(x=unlist(x_k1_k1)[1], 
-                                     y=unlist(x_k1_k1)[2], 
-                                     xVelocity=unlist(x_k1_k1)[3], 
-                                     yVelocity=unlist(x_k1_k1)[4],
-                                     xAcceleration=unlist(x_k1_k1)[5], 
-                                     yAcceleration=unlist(x_k1_k1)[6]))
+    x_k1_k <- Fm %*% x_k_k
+    P_k1_k <- Fm %*% P_k_k %*% t(Fm) + Qm
+    # Mise en mémoire Prediction
+    x_t1 <- append(x_t1, list(x_k1_k))
+    P_t1 <- append(P_t1, list(P_k1_k))
     
-    #xkk_values   <- rbind(xkk_values  , list(x=unlist(x_k_k  )[1], y=unlist(x_k_k  )[2]))
-    #xk1k_values  <- rbind(xk1k_values , list(x=unlist(x_k1_k )[1], y=unlist(x_k1_k )[2]))
-    #xk1k1_values <- rbind(xk1k1_values, list(x=unlist(x_k1_k1)[1], y=unlist(x_k1_k1)[2]))
+    # Correction
+    y_k1 <- yt[,i]
+    K <- (P_k1_k %*% t(Hm)) %*% inv(Hm %*% P_k1_k %*% t(Hm) + Rmatrix) 
+    x_k1_k1 <- as.vector(x_k1_k + K %*% (y_k1 - as.vector(Hmatrix %*% x_k1_k)))
+    P_k1_k1 <- (I - as.numeric(K %*% Hmatrix)) %*% P_k1_k
+    # Mise en mémoire Correction
+    x_tt <- append(x_tt, list(x_k1_k1))
+    P_tt <- append(P_tt, list(P_k1_k1))
 
     # Boucle
     P_k_k <- P_k1_k1
     x_k_k <- x_k1_k1
   }
 
-  #xkk_values   <<- xkk_values    
-  #xk1k_values  <<- xk1k_values   
-  #xk1k1_values <<- xk1k1_values  
-  
-  list(P_values, x_values)
+  # Renvoie une liste avec les valeurs de positions et variance/covariance, avant et apres correction
+  list(x_t1, P_t1, x_tt, P_tt, K)
 }
 
 
