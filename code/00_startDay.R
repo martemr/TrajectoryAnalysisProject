b11aa5b3d21f27ab7bfee081a3db179698a800f0
M. Martin EMERY <martin.emery@cerema.fr>
2023-07-17 12:11:34 +0200

    commentaires

--- code/00_startDay.R
+++ code/00_startDay.R
@@ -1,3 +1,11 @@
+##---------------------------------------------
+# Diagnostic SR via images aériennes
+# CEREMA
+# Author : Martin Emery
+# Date : July 2023
+# Description : Chargement des fonctions principales et des données.
+##---------------------------------------------
+
 source("code/01_init.r")
 source("code/02_plotUtils.R")
 
